49e9b1f9a6af7c76ab56d5e18f73a2f295de7786
TiD <nsofi2009@gmail.com>
2023-11-04 11:44:28 +0200

    classwork

--- main.py
+++ main.py
@@ -3,24 +3,29 @@
 #     satiety = 100
 #     age = 60
 #
+#
 # class Parent(Grandparent):
 #     age = 40
 #
+#
 # class Child(Parent):
 #     height = 90
+#
 #     def __init__(self):
 #         print(self.height)
-#         print(self.age)
 #         print(self.satiety)
+#         print(self.age)
+#
 #
 # nick = Child()
 
+
 class Hello_world:
-    hello = "Hello"
+    hello ="Hello"
     _hello = "_Hello"
     __hello = "__Hello"
 
-    def __init(self):
+    def __init__(self):
         self.world = "world"
         self._world = "_world"
         self.__world = "__world"
@@ -31,8 +36,13 @@ class Hello_world:
         print(self.world)
         print(self._world)
         print(self.__world)
-
-    # def __get_printer(self):
+    def _get_printer(self):
+        print(self.hello)
+        print(self._hello)
+        print(self.__hello)
+        print(self.world)
+        print(self._world)
+        print(self.__world)
 
 class Hi(Hello_world):
     def hi_print(self):
@@ -44,5 +54,6 @@ class Hi(Hello_world):
         print(self.__world)
 hello = Hello_world()
 hello.printer()
-hi = Hi()
-hi.hi_print()
+hello._get_printer()
+# hi = Hi()
+# hi.hi_print()
